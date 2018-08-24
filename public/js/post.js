170cfcb3cc70e5dd13d81377395265224c1811af
ccalilung <brobeans289@gmail.com>
2018-08-23 20:18:47 -0400

    post route backend

---
+++ public/js/post.js
@@ -0,0 +1,24 @@
+
+function function1() {
+let postInfo = {
+    //ADD THE LISTENER .val() stuff here!!
+    // title: "Honda NSX",
+    // body: "I need a right hand drive Honda NSX 2017"
+
+}
+$.ajax("/api/add_product/", {
+    type: "POST",
+    data: postInfo
+
+}).then(
+    function(data){
+      //update textbox with value returned from the database
+     console.log(data);
+       // $("#myauthor").val(data.author);
+        //$("#myquote").val(data.quote);
+
+        // location.reload();
+    });
+}
+
+function1();
