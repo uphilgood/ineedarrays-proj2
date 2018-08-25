5a99a23ec2c41f35ccd11e73bf46c6b7b9586e4c
ccalilung <brobeans289@gmail.com>
2018-08-25 13:40:50 -0400

    udpated post.js

--- public/js/post.js
+++ public/js/post.js
@@ -1,24 +1,66 @@
-$(function() {
-$("#submitCreate").on("click", function() {
+$(function () {
+    $("#submitCreate").on("click", function () {
 
-    event.preventDefault();
-    let postInfo = {
-    title: $("#post_title").val().trim(),
-    body: $("#post_body").val().trim(),
-    community: $("#commSelection").val()
+        event.preventDefault();
+        let postInfo = {
+            title: $("#post_title").val().trim(),
+            body: $("#post_body").val().trim(),
+            community: $("#commSelection").val()
 
-}
+        }
 
-console.log(postInfo)
+        console.log(postInfo)
 
-$.ajax("/api/add_product/", {
-    type: "POST",
-    data: postInfo
+        $.ajax("/api/add_product/", {
+            type: "POST",
+            data: postInfo
 
-}).then(
-    function(data){
-        location.replace("/")
-       
-    });
-})
+        }).then(
+            function (data) {
+                location.replace("/")
+
+            });
+    })
+
+    $("#createUser").on("click", function () {
+
+        event.preventDefault();
+        let postInfo = {
+            username: $("#newUsername").val().trim(),
+            password: $("#thePassword").val()
+
+        }
+
+        console.log(postInfo)
+
+        $.ajax("/createuser", {
+            type: "POST",
+            data: postInfo
+
+        }).then(function(){
+            
+
+            });
+    })
+
+    $("#login").on("click", function () {
+
+        event.preventDefault();
+        let postInfo = {
+            username: $("#newUsername").val().trim(),
+            password: $("#thePassword").val()
+
+        }
+
+        console.log(postInfo)
+
+        $.ajax("/loginuser", {
+            type: "POST",
+            data: postInfo
+
+        }).then(function(){
+            
+
+            });
+    })
 })
