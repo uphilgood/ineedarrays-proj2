29ba7bb661042c36e9b419eb55b30a6931774adb
Chris B <38058127+cbreeze28@users.noreply.github.com>
2018-09-02 17:55:54 -0400

    adjusting navbar/title

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 } else {
-var sequelize = new Sequelize("marketplace_db", "root", "Qaplzaml28!", {
+var sequelize = new Sequelize("marketplace_db", "root", "", {
   host: "localhost",
   port: 3306,
   dialect: "mysql",
